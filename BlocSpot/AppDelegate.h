dc173fc1c42527d0f57bd441e9242c6a91cc29c3
Philip Sopher <phsophe@gmail.com>
2015-08-21 12:50:30 -0400

    Initial Commit

---
+++ BlocSpot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocSpot
+//
+//  Created by Philip Sopher on 8/21/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocSpot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocSpot
+//
+//  Created by Philip Sopher on 8/21/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
